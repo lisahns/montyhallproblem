fa611a70877056d8d7dcf6b72556c30ef03a8a3c
lisahns <lisa.hanes@hotmail.de>
2022-06-14 12:13:57 +0100

    tidying up

--- apps.js
+++ apps.js
@@ -1,80 +1,55 @@
 const prizes = [
-    ["Goat", "Goat", "Car"],
-    ["Goat", "Car", "Goat"],
-    ["Car", "Goat", "Goat"]
+  ["Goat", "Goat", "Car"],
+  ["Goat", "Car", "Goat"],
+  ["Car", "Goat", "Goat"],
 ];
 
 let stickWin = 0;
 let switchWin = 0;
 
-
 // sw true = switch door
 // sw false = stick to door
 
-montyHall= (sw) => {
-    
-// pick prize selection randomly
-const pickedPrize = Math.floor(Math.random()*3);
-let prizeSelection = prizes[pickedPrize];
-// console.log(prizeSelection)
+montyHall = (sw) => {
+  // pick prize selection randomly
+  const pickedPrize = Math.floor(Math.random() * 3);
+  let prizeSelection = prizes[pickedPrize];
+  // console.log(prizeSelection)
 
-// pick door randomly
-const pickedDoor = Math.floor(Math.random()*3);
-// console.log(pickedDoor)
-let door = prizeSelection[pickedDoor];
-// console.log(door);
+  // pick door randomly
+  const pickedDoor = Math.floor(Math.random() * 3);
+  // console.log(pickedDoor)
+  let door = prizeSelection[pickedDoor];
+  // console.log(door);
 
-if (sw == true) {
+  if (sw == true) {
     //remove picked door
 
-const newDoor = prizeSelection.filter((x) => x !== prizeSelection[pickedDoor])
-// console.log(newDoor)
-//if picked door is goat, the door revealed will also be goat -> therefore only car is left
-//if picked door is car, and player chooses different door, loses either way
-if (newDoor =="Car"){
-    return switchWin +=1
-}
-}
+    const newDoor = prizeSelection.filter(
+      (x) => x !== prizeSelection[pickedDoor]
+    );
+    // console.log(newDoor)
+    //if picked door is goat, the door revealed will also be goat -> therefore only car is left
+    //if picked door is car, and player chooses different door, loses either way
 
-else if (sw == false && door == "Car") {
-    return stickWin +=1
+    if (newDoor == "Car") {
+      return (switchWin += 1);
     }
+  } else if (sw == false && door == "Car") {
+    return (stickWin += 1);
+  }
+};
 
-}
-
-// if (montyHall == "switchWin") {
-//     switchWin +=1
-//     console.log(switchWin)
-// }
-
-
-// if (montyHall == "stickWin") {
-//     stickWin +=1
-//     console.log(stickWin)
-// }
-
-
-// console.log(montyHall(false));
-// console.log(montyHall(true));
-
-//implement counter that stays
-//run function 100 times
-
-for (var i = 1; i < 1000; i++) montyHall(false);
-
-
-for (var i = 1; i < 1000; i++) montyHall(true);
-
-
-console.log(stickWin); 
-//this shows how many times out of 1000 stickWin was successful
-console.log(switchWin);
-//this shows how many times out of 1000 switchWin was successful
 
 //pass function 100 times with false; pass it 100 times with true
 // counter with return true or false
 
+for (var i = 1; i < 1000; i++) montyHall(false);
 
+for (var i = 1; i < 1000; i++) montyHall(true);
 
-
+console.log(stickWin);
+//this shows how many times out of 1000 stickWin was successful
+console.log(switchWin);
+//this shows how many times out of 1000 switchWin was successful
 
